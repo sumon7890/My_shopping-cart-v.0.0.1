8f5a35262338a9ca0fa0b12848c78dbe0b9fd93a
sumon7890 <sumon.ct.2019@gmail.com>
2021-04-17 14:59:50 +0600

    Running Project

--- index.js
+++ index.js
@@ -1,33 +1,160 @@
-// Phone Handle
-const shopingCardPlus = document.getElementById("shopingCardPlus").addEventListener("click", function () {
-    currentValuPlus("currentValue")
- });
-  const shopingCardminus = document.getElementById("shopingCardminus").addEventListener("click", function () { 
-    currentValuMains("currentValue");
- });
 
- // phone Cassing Handle 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Phone Handle
+// const shopingCardPlus = document.getElementById("shopingCardPlus").addEventListener("click", function () {
+//    const currentValue = document.getElementById("currentValue").value;
+//    const currentNumber = parseFloat(currentValue)+1;
+//    document.getElementById("currentValue").value = currentNumber; 
+   
+//     const currentPhonPrice = document.getElementById('currentPrice').innerText
+//     const currentPhonPriceNumber = parseFloat(currentPhonPrice) * currentNumber
+//        document.getElementById('currentPrice').innerText = currentPhonPriceNumber
+    
+//  });
+//   const shopingCardminus = document.getElementById("shopingCardminus").addEventListener("click", function () { 
+//    const currentValue = document.getElementById("currentValue").value;
+//    const currentNumber = parseFloat(currentValue)-1;
+//    document.getElementById("currentValue").value = currentNumber; 
+   
+//     const currentPhonPrice = document.getElementById('currentPrice').innerText
+//     const currentPhonPriceNumber = parseFloat(currentPhonPrice) * currentNumber;
+//        document.getElementById('currentPrice').innerText = currentPhonPriceNumber
+    
+//   })
+
+//  // phone Cassing Handle 
  
- const phoneCasePlus = document.getElementById("phoneCasePlus").addEventListener("click", function () {
-    currentValuPlus("currentValue2")
- });
- const phoneCaseMaines = document.getElementById("phoneCaseMaines").addEventListener("click", function () {
-    currentValuMains("currentValue2")
- });
- 
+//  const phoneCasePlus = document.getElementById("phoneCasePlus").addEventListener("click", function () {
+//    const currentValue2 = document.getElementById("currentValue2").value;
+//    const currentNumber2 = parseFloat(currentValue2)+1;
+//    document.getElementById("currentValue2").value = currentNumber2; 
+   
+//     const cureentPriceValueCassing = document.getElementById('cureentPriceValueCassing').innerText
+//     const cureentPriceValueCassingNumber = parseFloat(cureentPriceValueCassing) * currentNumber2
+//     console.log(cureentPriceValueCassing);
+//     document.getElementById('cureentPriceValueCassing').innerText = cureentPriceValueCassingNumber
+    
+//  });
+//  const phoneCaseMaines = document.getElementById("phoneCaseMaines").addEventListener("click", function () {
+//     currentValuMains("currentValue2")
+//  });
+// const subtotalAmaount = document.getElementById("subtotal").innerText;
+// const subtotaAmaountlNumber = parseInt(subtotalAmaount)
+// const subTotalAmaountValue = currentPhonPriceNumber + cureentPriceValueCassingNumber;
+// document.getElementById("subtotal").innerText = subTotalAmaountValue;
+
+
+
+
+//  const subTotal = document.getElementById('subTotal').value
+//  const totalSubPrice = parseFloat(subTotal);
+//  const totalSubPrice = cureentValueCassingNumber + currentPhonPriceNumber
+//  document.getElementById('subTotal').innerText = totalPrice ;
+// console.log(totalSubPrice)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // funcation plus / maines created
- function currentValuPlus(value){
-    const current = document.getElementById(value).value;
-    const currentNumber = parseInt(current)+1;
-    document.getElementById(value).value = currentNumber; 
+//  function currentValuPlus(value){
+//     const current = document.getElementById(value).value;
+//     const currentNumber = parseInt(current)+1;
+//     document.getElementById(value).value = currentNumber; 
     
- }
-  function currentValuMains(value){
-    const current = document.getElementById(value).value;
-    const currentNumber = parseInt(current)-1;
-    document.getElementById(value).value = currentNumber; 
+//  }
+//   function currentValuMains(value){
+//     const current = document.getElementById(value).value;
+//     const currentNumber = parseInt(current)-1;
+//     document.getElementById(value).value = currentNumber; 
     
- }
+//  }
+// subttoal 
 
+// function subtoalNumber(num){
+//    const cureentValueCassing = document.getElementById(num).innerText
+//     const currentCassingPriceNumber = parseFloat.apply(cureentValueCassing) * currentValue2
+//     document.getElementById(num).innerText = currentCassingPriceNumber
+// }
 
 
